906576f7512a290d779a0d4a5ce5aaadba4e7dc6
ldjimenezt <ldjimenezt@utn.edu.ec>
2024-06-26 20:55:17 -0500

    add Montserrat & rm img

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,4 @@
-import { JetBrains_Mono } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 
 // Components
@@ -6,10 +6,10 @@ import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
 import StairTransition from "@/components/StairTransition";
 
-const jetbrainsMono = JetBrains_Mono({
+const montserrat = Montserrat({
   subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
-  variable: "--font-jetbrainsMono",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-montserrat",
 });
 
 export const metadata = {
@@ -20,7 +20,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={jetbrainsMono.variable}>
+      <body className={montserrat.variable}>
         <Header />
         <StairTransition />
         <PageTransition>{children}</PageTransition>
